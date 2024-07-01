0d8a0c21caaf953b73f9a817d6481931362f2318
kirsanovmike <kirsanovmike@gmail.com>
2024-07-02 01:11:28 +0300

    fix header and scroll to top

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <!-- Системный навбар -->
-    <v-container fluid class="pt-2 pb-2">
+    <v-container id="top" class="pt-2 pb-2" fluid>
       <v-row>
-        <v-spacer />
-        <v-col cols="10" class="d-flex justify-end align-center">
+        <v-spacer/>
+        <v-col class="d-flex justify-end align-center" cols="10">
           <!-- Скачать резюме -->
           <div class="d-flex align-center ml-8">
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="text-info"  />
+            <font-awesome-icon :icon="['fas', 'chevron-down']" class="text-info"/>
             <span class="font--callout-2 ml-1 text-info">Download CV</span>
           </div>
           <!-- Смена языка -->
@@ -22,45 +22,54 @@
             @click="switchTheme"
           />
         </v-col>
-        <v-spacer />
+        <v-spacer/>
       </v-row>
-    </v-container>
-    <v-container>
     </v-container>
     <!-- Системный навбар -->
     <v-app-bar
-      class="mt-10 background"
+      class="background"
       elevation="0"
-      absolute="true"
     >
       <v-container fluid>
         <v-row>
-          <v-spacer />
+          <v-spacer/>
           <v-col cols="10">
-            <nav-list :items="indexStore.getMenu" :selected-id="selectedId" @selectMenuOption="goToElem($event)" />
+            <nav-list :items="indexStore.getMenu" :selected-id="selectedId" @selectMenuOption="goToElem($event)"/>
             <!-- <nav-list :items="links" /> -->
           </v-col>
-          <v-spacer />
+          <v-spacer/>
         </v-row>
       </v-container>
     </v-app-bar>
     <v-main>
-      <RouterView />
+      <RouterView/>
+      <v-btn
+        v-scroll="onScroll"
+        v-show="fab"
+        color="yellow"
+        icon
+        style="position: fixed; bottom: 32px; right: 32px"
+        @click="toTop"
+      >
+        <font-awesome-icon
+          style="max-width: 32px; max-height: 32px;"
+          icon="chevron-up"
+          color="info"
+        />
+      </v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-import {type ComputedRef, ref, watch} from 'vue'
+import {computed, type ComputedRef, onBeforeMount, ref, shallowRef, watch} from 'vue'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {useUserStore} from "@/stores/user";
 import {useIndexStore} from "@/stores/index";
-import {computed, onMounted, onBeforeMount } from "vue";
 import {getUserId} from "@/api/user";
-import {useTheme} from "vuetify";
+import {useGoTo, useTheme} from "vuetify";
 import ChangeLanguage from "@/components/ChangeLanguage.vue";
 import NavList from "@/components/common/NavList.vue";
-import { useGoTo } from 'vuetify'
 import router from "@/router";
 
 const theme = useTheme();
@@ -103,33 +112,55 @@ const systemTheme = computed(() =>
 
 watch(systemTheme, (systemTheme) => {
   if (systemTheme) {
-    theme.global.name.value = themeNames[0]
+    theme.global.name.value = themeNames[0];
+    themeCounter.value = 0;
   } else {
-    theme.global.name.value = themeNames[1]
+    theme.global.name.value = themeNames[1];
+    themeCounter.value = 1;
   }
 }, {
   immediate: true
 })
 
 const switchTheme = () => {
+  theme.global.name.value = themeNames[(themeCounter.value + 1) % themeNames.length];
   themeCounter.value++;
-  theme.global.name.value = themeNames[themeCounter.value % themeNames.length];
 };
 
 const defId: ComputedRef<string> = computed(() => indexStore?.getMenu?.[0]?.id)
 
 let selectedId = ref<string>("");
 watch(defId, (newValue) => {
-  if (newValue != null) {
-    selectedId.value = newValue;
-  }
-},
+    if (newValue != null) {
+      selectedId.value = newValue;
+    }
+  },
   {immediate: true}
 )
 
 
-const goToElem = (itemId:string) => {
-  router.push({ query: { block: itemId }})
+const goToElem = (itemId: string) => {
+  router.push({query: {block: itemId}})
   selectedId.value = itemId;
 }
+let fab = ref(false);
+const onScroll = (e: any) => {
+  if (typeof window === 'undefined') return
+  const top = window.scrollY || e.target.scrollTop || 0
+  fab.value = top > 20
+}
+const goTo = useGoTo()
+const responsive = ref()
+const toTop = () => {
+  document.getElementById("top")?.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
 </script>
+
+<style lang="scss">
+.v-app-bar.v-toolbar {
+  position: sticky !important;
+  top: 0;
+}
+</style>
